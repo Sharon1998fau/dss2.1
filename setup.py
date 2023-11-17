ce1085085ae4b90faaf36eddd48196459e239c61
Sharon1998fau <sharonvarghesearakkal@gmail.com>
2023-11-17 11:15:42 +0100

    final

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='Sharon',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='https://github.com/Sharon1998fau/dss2.1.gitt',
+      author='Sharon',
+      author_email='Sharonvarghesearakkal@gmail.com',
+      license='MIT',
+      packages=['Mathquiz'],
+      zip_safe=False)
